488293153495300e2feaf2de4451994049576840
Balázs Izsóf <balazs.izsof@gmail.com>
2021-03-07 13:46:56 +0100

    start main flow of application

--- app/src/main/java/com/bb_score/BusinessLogic.java
+++ app/src/main/java/com/bb_score/BusinessLogic.java
@@ -1,5 +1,8 @@
 package com.bb_score;
 
+import android.os.Build;
+import androidx.annotation.RequiresApi;
+import com.bb_score.industry_facility.IndustryFacilitiesProvider;
 import com.bb_score.location.Location;
 
 import java.util.HashMap;
@@ -8,6 +11,7 @@ import java.util.Set;
 
 public class BusinessLogic {
     private final Set<Location> locations;
+    private IndustryFacilitiesProvider industryFacilitiesProvider;
     private final HashMap<Player, Integer> scores = new HashMap<Player, Integer>();
 
     public BusinessLogic(Set<Location> locations) {
@@ -16,5 +20,34 @@ public class BusinessLogic {
 
     public Map<Player, Integer> calculateScore(){
         return scores;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void loadAll(){
+        loadIndustryFacilities();
+        loadLinks();
+        loadLocations();
+        loadMerchantPoints();
+    }
+
+    private void loadMerchantPoints() {
+        throw new UnsupportedOperationException();
+    }
+
+    private void loadLocations() {
+        throw new UnsupportedOperationException();
+    }
+
+    private void loadLinks() {
+        throw new UnsupportedOperationException();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void loadIndustryFacilities(){
+        this.industryFacilitiesProvider = new IndustryFacilitiesProvider("industry_facilities.json");
     }
 }
