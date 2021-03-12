174491416fd2cedeeb980e29bc5f47aa7ec8b2b8
Balázs Izsóf <balazs.izsof@gmai.com>
2021-03-12 17:10:42 +0100

    #6 add test to check whether POJO parsed correctly from JSON

--- app/src/test/java/com/bb_score/deserializer/JacksonDeserializerTest.java
+++ app/src/test/java/com/bb_score/deserializer/JacksonDeserializerTest.java
@@ -2,6 +2,7 @@ package com.bb_score.deserializer;
 
 import com.bb_score.industry_facility.IndustryFacility;
 import com.bb_score.industry_facility.IndustryFacilityType;
+import com.bb_score.industry_facility.IndustryFacilityType.*;
 import com.bb_score.link.Link;
 import com.bb_score.location.Location;
 import org.junit.jupiter.api.AfterEach;
@@ -9,7 +10,10 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.util.Arrays;
 import java.util.List;
+
+import static com.bb_score.industry_facility.IndustryFacilityType.*;
 
 class JacksonDeserializerTest {
     private JacksonDeserializer<?> deserializer;
@@ -23,7 +27,8 @@ class JacksonDeserializerTest {
 
     @Test
     void testPOJOReturnedAfterLocationsJsonParsed() {
-        assertListFirstELementWithGivenPOJO("Locations.json", Location.class, new Location("STOKE-ON-TRENT"));
+        IndustryFacilityType[][] slots = {{COTTON_MILL, MANUFACTURER}, {POTTERY, IRON_WORK}, {MANUFACTURER}};
+        assertListFirstELementWithGivenPOJO("Locations.json", Location.class, new Location("STOKE-ON-TRENT", slots));
     }
 
     @Test
@@ -33,7 +38,7 @@ class JacksonDeserializerTest {
 
     @Test
     void testPOJOReturnedAfterIndustryFacilitiesJsonParsed() {
-        expected = new IndustryFacility(IndustryFacilityType.MANUFACTURER,1, 3,2);
+        expected = new IndustryFacility(IndustryFacilityType.MANUFACTURER, 1, 3, 2);
         assertListFirstELementWithGivenPOJO("IndustryFacility.json", IndustryFacility.class, expected);
     }
 
